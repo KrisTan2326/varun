65d3066d97818f7e4ef8c0f2241d1e85cd04c9da
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-10-14 16:41:40 +0530

    Update ScheduledTasks.java

--- ScheduledTasks.java
+++ ScheduledTasks.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.scheduling.annotation.Scheduled;
+
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -28,5 +29,6 @@ public class ScheduledTasks {
     }
 
 }
+
 
 
